587dcdd25d2168a4d7e0818be1a36ff63083f39a
samenoko-dayo <kumagai7554@gmail.com>
2026-04-12 08:19:39 +0900

    アイコンにaltを設定

--- app/components/profile.vue
+++ app/components/profile.vue
@@ -9,7 +9,8 @@
             管理人
         </h2>
         <div class="flex flex-row items-center gap-4">
-            <NuxtImg src="/images/icon.jpg" class="w-16 h-16 rounded-full border-4 border-slate-200 self-start" />
+            <NuxtImg src="/images/icon.jpg" alt="アイコン"
+                class="w-16 h-16 rounded-full border-4 border-slate-200 self-start" />
             <div class="flex flex-col">
                 <span class="font-bold">samenoko-dayo</span>
                 <span class="text-sm text-slate-600 font-medium mt-1">18歳の無職です。<br>Pythonとフロントエンドを少しだけ書きます。</span>
