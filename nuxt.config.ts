f6339c78c162a495a1d7b294f09ad192e44f7573
samenoko-dayo <kumagai7554@gmail.com>
2026-04-11 14:53:51 +0900

    set baseurl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   css: ["~/assets/css/main.css"],
   devtools: { enabled: true },
+  app: {
+    baseURL: "https://samenoko.work"
+  },
 
   nitro: {
     prerender: {
